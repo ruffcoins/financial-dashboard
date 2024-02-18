11742352e0c6c10f2bbd4213a86cc2a6bda07760
Anthony Chukwujekwu <nzubeanthony4@gmail.com>
2024-02-18 03:29:42 +0100

    bug fix

--- components/shared/ProgressMeter.tsx
+++ components/shared/ProgressMeter.tsx
@@ -4,7 +4,7 @@ interface ProgressMeterProps {
 
 const ProgressMeter: React.FC<ProgressMeterProps> = ({ percent }) => {
   return (
-    <div className="relative pt-1">
+    <div className="pt-1">
       <div className="w-full rounded-full bg-gray-200">
         <div
           className={`rounded-full py-2 bg-primary-900`}
